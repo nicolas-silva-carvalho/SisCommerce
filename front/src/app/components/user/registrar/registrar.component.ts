5c85c39bdf016fa1a9c28a2fe4c192c74ec44a28
nicolas.carvalho <nicolas.carvalho@cgm.fortaleza.ce.gov.br>
2023-11-28 17:30:35 -0300

    show

--- front/src/app/components/user/registrar/registrar.component.ts
+++ front/src/app/components/user/registrar/registrar.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../../models/identity/User';
 import { UserServiceService } from '../../../services/UserService.service';
 import { Router, RouterOutlet } from '@angular/router';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,41 +17,48 @@ import { CommonModule } from '@angular/common';
   templateUrl: './registrar.component.html',
   styleUrls: ['./registrar.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule]
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
 })
 export class RegistrarComponent implements OnInit {
-
   user = {} as User;
   form!: FormGroup;
 
-  constructor(private userService: UserServiceService, private router: Router, private formBuilder: FormBuilder) { }
+  constructor(
+    private userService: UserServiceService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
-  get f(): any { return this.form.controls; }
+  get f(): any {
+    return this.form.controls;
+  }
 
   ngOnInit() {
     this.validation();
   }
 
   private validation(): void {
+
     this.form = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]],
       username: ['', Validators.required],
       primeiroNome: ['', Validators.required],
-      ultimoNome: ['', Validators.required]
-      //confirmarSenha: ['', [Validators.required, Validators.minLength(4)]]
-  });
+      ultimoNome: ['', Validators.required],
+      confirmarSenha: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
 
   register(): void {
     this.user = { ...this.form.value };
     this.userService.registrar(this.user).subscribe({
-      next: () => {this.router.navigateByUrl('/produtos'); },
+      next: () => {
+        this.router.navigateByUrl('/produtos');
+      },
       error: (error: any) => {
         if (error.status == 401) console.log('Usuário ou senha inválidos');
         else console.log(error);
-      }
-    })
+      },
+    });
   }
 }
-
